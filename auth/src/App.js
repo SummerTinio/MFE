c98c2e5e8f9adb1c0e7ed9d51e3b7e9ed15ba655
Summer Tinio <79723484+SummerTinio@users.noreply.github.com>
2021-06-20 10:27:27 +0800

    receive onSignIn on Auth App, pass down via Routes

--- auth/src/App.js
+++ auth/src/App.js
@@ -20,15 +20,20 @@ const generateClassName = createGenerateClassName({
   productionPrefix: 'auth',
 });
 
-const App = function AuthComponent({ history }) {
+const App = function AuthComponent({ history, onSignIn }) {
   return (
+    // eslint-disable-next-line react/jsx-filename-extension
     <>
       <StylesProvider generateClassName={generateClassName}>
         <Router history={history}>
           <Route exact path="/" component={SignIn} />
           <Switch>
-            <Route path="/auth/signin" component={SignIn} />
-            <Route path="/auth/signup" component={SignUp} />
+            <Route path="/auth/signin">
+              <SignIn onSignIn={onSignIn} />
+            </Route>
+            <Route path="/auth/signup">
+              <SignUp onSignIn={onSignIn} />
+            </Route>
           </Switch>
         </Router>
       </StylesProvider>
